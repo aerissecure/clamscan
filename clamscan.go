9d06c5e633a922f609ebf6b3e902b8dfe5c3242f
Jeffrey Stiles <freb@users.noreply.github.com>
2022-01-11 17:05:47 -0800

    Convert to library and add example code

--- clamscan.go
+++ clamscan.go
@@ -1,17 +1,20 @@
-package main
+package clamscan
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"fmt"
 	"io"
-	"os"
 	"os/exec"
 	"regexp"
+	"strings"
+	"time"
 )
 
 const (
-	clamscanExe  = "clamscan"
-	clamdscanExe = "clamdscan"
+	ClamscanExe  = "clamscan"
+	ClamdscanExe = "clamdscan"
 )
 
 var (
@@ -26,98 +29,115 @@ func virusName(stdout string) string {
 	return matches[1]
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("missing required file arguments")
-		os.Exit(1)
+type Engine struct {
+	exe string
+}
+
+type Version struct {
+	ClamAVVersion    string
+	SignatureVersion string
+	SignatureDate    time.Time
+}
+
+func New(exe string) (Engine, error) {
+	// Systemd will automatically start clamav-daemon through its socket in most
+	// cases, so there isn't a good (dependency free) way to check if clamdscan
+	// is running without starting it.
+
+	e := Engine{}
+
+	path, err := exec.LookPath(exe)
+	if err != nil {
+		return e, err
 	}
 
-	exe := ""
+	// Use full path
+	e.exe = path
 
-	if _, err := exec.LookPath("clamscan"); err == nil {
-		exe = clamscanExe
+	return e, nil
+}
+
+// Parses the result of `clamscan --version`, and parses result of the form:
+// `0.104.1/26419/Tue Jan 11 01:24:18 2022`
+func (e Engine) Version() (Version, error) {
+	cmd := exec.Command(e.exe, "--version")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return Version{}, err
 	}
 
-	if _, err := exec.LookPath("clamdscan"); err == nil {
-		exe = clamdscanExe
+	ver := strings.TrimSpace(string(out))
+	fields := strings.Split(ver, "/")
+
+	if len(fields) < 3 {
+		return Version{}, fmt.Errorf("unexpected output: %s", ver)
 	}
 
-	if exe == "" {
-		fmt.Printf("%s / %s executables not found\n", clamscanExe, clamdscanExe)
-		os.Exit(1)
+	v := Version{
+		ClamAVVersion:    fields[0],
+		SignatureVersion: fields[1],
 	}
 
-	for _, f := range os.Args[1:] {
-		file, err := os.Open(f)
-		if err != nil {
-			fmt.Printf("Error opening %s: %v\n", f, err)
-			continue
-		}
-
-		cmd := exec.Command(exe, "-")
-		stdin, err := cmd.StdinPipe()
-		if err != nil {
-			fmt.Printf("Error opening stdin (%s): %v\n", f, err)
-		}
-		// defer stdin.Close()
-
-		var stdout, stderr bytes.Buffer
-		cmd.Stdout = &stdout
-		cmd.Stderr = &stderr
-
-		go func() {
-			defer stdin.Close()
-			io.Copy(stdin, file)
-		}()
-
-		err = cmd.Run()
-		ec := -1
-
-		if err == nil {
-			// Here when everything was OK with the command and 0 return code
-			fmt.Println("NO VIRUS FOUND")
-			os.Exit(0)
-		}
-
-		if err != nil {
-			exitErr, ok := err.(*exec.ExitError)
-			if ok {
-				ec = exitErr.ExitCode()
-			}
-			if !ok {
-				fmt.Printf("Error running %s: %v", exe, err)
-				os.Exit(1)
-			}
-		}
-
-		// fmt.Println("exit code:", ec)
-
-		if ec == 0 {
-			// Here when everything was was not OK with the command and 0 return code
-			fmt.Println("NO VIRUS FOUND")
-			os.Exit(0)
-		}
-
-		if ec == 1 {
-			fmt.Println("FOUND VIRUS:", virusName(stdout.String()))
-		}
-
-		// We want to check stderr:
-
-		// 0 : No virus found.
-		// 1 : Virus(es) found.
-		// 2 : Some error(s) occured.
-
-		// TODO: Not sure what to do with returne stderr content
-		// if stderr.String() != "" {
-		// 	fmt.Println("ERROR:", stderr.String())
-		// 	os.Exit(1)
-		// }
-
-		// fmt.Println("STDOUT:")
-		// fmt.Println(stdout.String())
-		// fmt.Println()
-
+	date, err := time.Parse("Mon Jan 02 15:05:05 2006", fields[2])
+	if err != nil {
+		return v, err
 	}
 
+	v.SignatureDate = date
+	return v, nil
+}
+
+func (e Engine) Scan(file io.Reader) (infected bool, name string, err error) {
+	return e.ScanContext(context.Background(), file)
+}
+
+func (e Engine) ScanContext(ctx context.Context, file io.Reader) (infected bool, name string, err error) {
+	cmd := exec.CommandContext(ctx, e.exe, "-")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return false, "", err
+	}
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	go func() {
+		defer stdin.Close()
+		io.Copy(stdin, file)
+	}()
+
+	ec := -1
+	err = cmd.Run()
+
+	if err == nil {
+		// Everything was OK with the command and return code is 0
+		return false, "", nil
+	}
+
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			// Exit codes:
+			// 0 : No virus found.
+			// 1 : Virus(es) found.
+			// 2 : Some error(s) occured.
+			ec = exitErr.ExitCode()
+		} else {
+			// Unknown error occured
+			return false, "", fmt.Errorf("unknown error: %w", err)
+		}
+	}
+
+	if ec == 1 {
+		// Note, empty virus name is not currently handled.
+		return true, virusName(stdout.String()), nil
+	}
+
+	// If ec==0, everything was was NOT OK with the command and return code is 0.
+	if ec == 0 && stdout.Len() == 0 {
+		return false, "", nil
+	}
+
+	// Consider everything else an error.
+	return false, "", errors.New(stdout.String())
 }
---
+++ cmd/clamit/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"clamscan"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	engine, err := clamscan.New(clamscan.ClamdscanExe)
+	if err != nil {
+		fmt.Println("Error creating engine:", err)
+		os.Exit(1)
+	}
+	file, _ := os.Open(os.Args[1])
+	infected, name, err := engine.Scan(file)
+	fmt.Println("Infected:", infected)
+	fmt.Println("Name:", name)
+	fmt.Println("Error:", err)
+
+	v, err := engine.Version()
+	fmt.Println("ClamAVVersion:", v.ClamAVVersion)
+	fmt.Println("SignatureVersion:", v.SignatureVersion)
+	fmt.Println("SignatureDate:", v.SignatureDate)
+	fmt.Println("Error:", err)
+}
